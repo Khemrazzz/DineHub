d13aa03f722d0deb49aa7b0ff0aee30cca1cbde1
Shar5K_95 <145230570+Khemrazzz@users.noreply.github.com>
2025-08-07 17:14:40 +0400

    chore: update ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.example.dinehub"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // Pin the Android NDK version to satisfy plugin requirements
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
